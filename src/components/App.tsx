3bfedd7a8e708f3926943460236d1be81183edbe
Jesper Olsson <jesperolsson@icloud.com>
2019-05-03 16:26:43 +0200

    Indentation fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import './css/style.css'
 
-export interface AppProps { compiler: string; framework: string; }
+interface AppProps {
+  compiler: string; 
+  framework: string; 
+}
 
 export class App extends React.Component<AppProps, {}> {
-    render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
-    }
+  render() {
+    return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+  }
 }
